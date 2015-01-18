7d8d388d8d131326d8839031ba53317409017cd0
Tamwyn <tamwyn.eopia@live.de>
2015-01-18 17:17:50 +0100

    Datenbanknamen aktualisiert

--- subpages/AddFencer.php
+++ subpages/AddFencer.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "fechten";
+$db_name = "turniermanagment";
 
 $errors = array();
 $Waobjects = array ("saebel", "florett");
--- subpages/Tournament.php
+++ subpages/Tournament.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "fechten";
+$db_name = "turniermanagment";
 
 $errors = array();
 $AKobjects = array ("schueler", "bjg" , "ajg" , "jun" , "aktive" , "senioren"  );//Die Namen der Checkboxen fuer Kontrollen
